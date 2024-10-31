bfba79ec97a8a793f67dc114df86e2bd03eb57e3
bsvcode <bvaleiro@gmail.com>
2024-10-31 20:37:11 -0300

    feature:change cache no next

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,5 +4,5 @@ import { revalidateTag } from "next/cache";
 export async function POST() {
   revalidateTag("prismic");
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: false, now: Date.now() });
 }
