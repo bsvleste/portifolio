7b434db59a6538c93b3e2c08a0a13b5b8dcbdbb8
bsvcode <bvaleiro@gmail.com>
2024-10-31 21:01:42 -0300

    fix:revalidateTag

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function POST() {
-  //  revalidateTag("prismic");
-  const pathsToRevalidate = ['/','/blog', '/projetos']; // Add paths to revalidate
-  await Promise.all(pathsToRevalidate.map((path) => revalidatePath(path)));
-  return NextResponse.json({ revalidated: true });
+  revalidateTag("prismic");
+
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
